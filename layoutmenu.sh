b5a032d698dca18a86fa8f2da96e1fbee312c8b9
Plasmoduck <chrisjoelgarvin@gmail.com>
2020-11-30 16:42:59 +1100

    added layoutmenu.sh to be used with xmenu, cp to /home/cjg/bin:/usr/local/bin:/home/cjg/bin:/sbin:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/usr/bin/env

---
+++ layoutmenu.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cat <<EOF | xmenu
+[\\] Fibonacci	0
+|M| Centered	1
+><> Floating	2
+[]= Tiled	3
+||| Columns	4
+>M> CFM		5
+[M] Monocle	6
+[D] Deck	7
+TTT Bstack	8
+=== BstrackHoriz	9
+::: Grid	10
+(@) Spiral	11
+|+| Tatami	12
+EOF
