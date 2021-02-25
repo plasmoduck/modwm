e4eb06b0a05dbde6a247b04746cf8054b5c8087e
plasmoduck <chrisjoelgarvin@gmail.com>
2021-02-26 00:30:32 +1100

    Big updates for statusbar.sh - restructured & using trap

--- statusbar.sh
+++ statusbar.sh
@@ -1,21 +1,18 @@
 #!/bin/sh -e
-#  _____ _        _             _
-# /  ___| |      | |           | |
-# \ `--.| |_ __ _| |_ _   _ ___| |__   __ _ _ __
-#  `--. | __/ _` | __| | | / __| '_ \ / _` | '__|
-# /\__/ | || (_| | |_| |_| \__ | |_) | (_| | |
-# \____/ \__\__,_|\__|\__,_|___|_.__/ \__,_|_|
-#
-# MODDWM STATUSBAR - Modular Dynamic Window Manager Statusbar Gruvbox Flavour.
-# ----------------------------------------------------------------------------
-#  "THE BEER-WARE LICENSE" (Revision 42):
-#  Plasmoduck <plasmoduck@gmail.com> wrote this file.  As long as you retain this notice you
-#  can do whatever you want with this stuff. If we meet some day, and you think
-#  this stuff is worth it, you can buy me a beer in return - Plasmoduck.
-# ---------------------------------------------------------------------------- 
+
+pidfile=/tmp/statbar-$(id -u)-$DISPLAY
+
+printf %s "$$" >"$pidfile"
+
+trap update TRAP
+
+# set the deliminter character
+DELIM=' '
 
 cpu (){
-       vmstat | awk 'NR==3 {print $(NF-2)}'     # See man vmstat(1)
+        _cpu=$(vmstat | awk 'NR==3 {print $(NF-2)}')     # See man vmstat(1)
+        _cpuicon=
+        printf ^c#FB4934^%s^c#D5C4A1^%s "$_cpuicon" "$_cpu"% "$DELIM"
 }
 
 cpu_temp (){
@@ -25,15 +22,19 @@ cpu_temp (){
         elif test "$_temp" -ge 1; then
                 _tempicon=
         fi
-        printf ^c#E78A4E^%s^c#D5C4A1^%s "$_tempicon" "$_temp"°
+        printf ^c#E78A4E^%s^c#D5C4A1^%s "$_tempicon" "$_temp"° "$DELIM"
 }
 
 memory (){
-	free | awk '(NR == 18) {print $6}'      # free is a perl script to show free ram on FreeBSD.
+	_memory=$(free | awk '(NR == 18) {print $6}')      # free is a perl script to show free ram on FreeBSD.
+        _memoryicon=
+        printf ^c#FABD2F^%s^c#D5C4A1^%s "$_memoryicon" "$_memory" "$DELIM"
 }
 
 drive (){
-	df -h | grep '/$' | awk '{print $5}'
+	_drive=$(df -h | grep '/$' | awk '{print $5}')
+        _driveicon=
+        printf ^c#B8BB26^%s^c#D5C4A1^%s "$_driveicon" "$_drive" "$DELIM"
 }
 
 volume (){
@@ -47,7 +48,7 @@ volume (){
         elif test "$_vol" -eq 0; then
                 _volicon=
         fi
-        printf ^c#83A598^%s^c#D5C4A1^%s%% "$_volicon" "$_vol"
+        printf ^c#83A598^%s^c#D5C4A1^%s "$_volicon" "$_vol"% "$DELIM"
 }
 
 battery(){
@@ -64,20 +65,51 @@ battery(){
 			_batticon=
 		fi
 	fi
-	printf ^c#8EC07C^%s^c#D5C4A1^%s%% "$_batticon" "$_battperc"
+	printf ^c#8EC07C^%s^c#D5C4A1^%s "$_batticon" "$_battperc"% "$DELIM"
 }
 
-print_date (){
-	date "+%H:%M"
+weather() {
+        LOCATION=Granville,NSW     # Set your location.
+        find ~/.cache/weather.txt '!' -newermt '1 hour ago' -exec curl -s -o '{}' wttr.in/$LOCATION?format=1 ';'
+        _weather=$(printf "%s\n""$SEP2""$(tr -d ' ' < ~/.cache/weather.txt)")
+        printf ^c#D5C4A1^%s "$_weather" "$DELIM"
 }
+                                
 
 wifi (){
 #       ifconfig wlan0 | grep ssid | cut -w -f 3        # Print wireless SSID name.
-        ifconfig wlan0 | grep txpower | cut -w -f 3     # Print wireless SSID signal strength.
+        _wifiperc=$(ifconfig wlan0 | grep txpower | cut -w -f 3)     # Print wireless SSID signal strength.
+        _wifiicon=
+        printf ^c#665C54^%s^c#D5C4A1^%s "$_wifiicon" "$_wifiperc"% "$DELIM"
 }
 
-while true
-do
-    dwm -s "^b#322F2E^^c#FB4934^^c#D5C4A1^$(cpu)% $(cpu_temp) ^c#FABD2F^^c#D5C4A1^$(memory) ^c#B8BB26^^c#D5C4A1^$(drive) $(volume) $(battery) ^c#665C54^^c#D5C4A1^$(wifi)% ^c#D3869B^^c#D5C4A1^$(print_date)"
-	sleep 5
+datetime (){
+        _date=$(date "+%H:%M")
+        _dateicon=
+        printf ^c#D3869B^%s^c#D5C4A1^%s "$_dateicon" "$_date"
+}
+
+status()
+{
+        cpu
+        cpu_temp
+        memory
+        drive
+        volume
+        battery
+        wifi
+        weather
+        datetime
+}
+
+update()
+{
+        dwm -s "$(status)" &
+        wait
+}
+
+while :; do
+        update
+        sleep 60 &
+        wait
 done
